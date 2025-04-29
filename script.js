98eed02edf8235e65f9a79464e55d83e1ecb7ac4
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-29 13:33:52 -0700

    write pseudo code for three necessary functions
    lays out steps to do for user inputted new grids

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const gridContainer = document.querySelector('#gridContainer');
+const newGridButton = document.querySelector('#newGridButton');
+
+newGridButton.addEventListener('click', () => {
+    //anonymous function
+    //prompt user for input number
+        //from 1-100
+        //call generation function with input num as side length
+});
 
 const generatePixel = (id) => {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
     pixel.id = id;
+
+    //add optional sideLength param
+    //if sideLength exists
+        //change height/width to 100/sideLength
+
     gridContainer.appendChild(pixel);
     pixel.addEventListener('mouseover', () => {
         pixel.style.backgroundColor = 'black';
     });
 }
+
+//DECLARE function that creates new grid, with side length param
+//for loop from 0 to side length squared
+//generate pixels up to that amount, with side length arg
+
 
 for (let i = 0; i < 256; i++) {
     generatePixel(i+1);
