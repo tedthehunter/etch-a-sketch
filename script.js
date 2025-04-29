2615540d504607ec3f9873b46330e96c2b948af7
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-29 12:31:42 -0700

    write generatePixel function
    creates a pixel and adds to gridContainer

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//write function to create square div
-    //create new div, store to variable
-    //add class to div
-    //append div to div container
+//store gridcontainer in variable
+const generatePixel = () => {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    gridContainer.appendChild(pixel);
+}
 
 //write for loop to call creation function 16x16 times
