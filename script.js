3a79d821946af4d79726c0b54ca7fb7c532fac80
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-29 12:28:36 -0700

    write pseudocode for creation function, basic style
    boilerplate for creating and testing divs

---
+++ script.js
@@ -0,0 +1,6 @@
+//write function to create square div
+    //create new div, store to variable
+    //add class to div
+    //append div to div container
+
+//write for loop to call creation function 16x16 times
