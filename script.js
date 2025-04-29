91c5f6785aad4d2410c38159f871c1b3146281a1
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-29 12:32:35 -0700

    store gridcontainer and call creation function
    test successful

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-//store gridcontainer in variable
+const gridContainer = document.querySelector('#gridContainer');
+
 const generatePixel = () => {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
     gridContainer.appendChild(pixel);
 }
 
+generatePixel();
 //write for loop to call creation function 16x16 times
